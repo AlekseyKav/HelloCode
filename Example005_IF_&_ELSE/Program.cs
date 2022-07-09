c826e91c7cf6a4597493f68147cd23f528b58d4c
ALeksey <nor-man@yandex.ru>
2022-07-09 11:59:32 +0300

    Привет, Марина!

---
+++ Example005_IF_&_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "марина")
+{
+    Console.WriteLine("Ура это же Марина!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!");
+}
