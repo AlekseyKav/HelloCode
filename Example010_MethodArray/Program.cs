f28608b8c84f2dab63f76c217e78d25cf2f08f65
ALeksey <nor-man@yandex.ru>
2022-07-12 13:11:31 +0300

    add Ex_010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1,63,21,3,14,5,63,7,18};
+
+int n = array.Length; //определение длинны массива//
+int find =63;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]== find)
+    {
+        Console.WriteLine(index);
+        break; //операция прерывания, т.е. нашли первый совпадающий элемент и на этом остновились//
+    }
+    index ++; // index = index + 1 //
+
+}
